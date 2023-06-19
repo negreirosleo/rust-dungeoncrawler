75ca88b9929e65547dfef77f467f83dd8837ce13
Leonardo Negreiros de Oliveira <negreirosleo12@gmail.com>
2023-06-19 20:53:38 -0300

    Add ECS to the project

---
+++ src/components.rs
@@ -0,0 +1,22 @@
+use crate::prelude::*;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Player;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Enemy;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct MovingRandomly;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct WantsToMove {
+    pub entity: Entity,
+    pub destination: Point,
+}
